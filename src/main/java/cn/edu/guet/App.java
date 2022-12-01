efd299d8ea2eb4168d5bd3f8b605748db7573ab4
Abby <1308286019@qq.com>
2022-12-01 14:48:49 +0800

    练习

---
+++ src/main/java/cn/edu/guet/App.java
@@ -0,0 +1,13 @@
+package cn.edu.guet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
